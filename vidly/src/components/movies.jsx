1079035bc85dcd02e454bf9bb15c6ae5027612f3
MartinMSalas <ingmartinmsalas@gmail.com>
2022-10-17 12:47:43 -0300

    creating listgroup

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
+/*
+npm install --save react-bootstrap
+
+import {Grid, Row, Col} from "react-bootstrap";
+*/
+import { Row, Col } from "react-bootstrap";
 /* 
 install font awesome react https://fontawesome.com/v5/docs/web/use-with/react#get-started
 npm i --save @fortawesome/fontawesome-svg-core
@@ -19,10 +25,10 @@ const emptyHeart = <FontAwesomeIcon icon={farHeart} />;
 import Like from "./common/like";
 import Pagination from "./common/pagination";
 
-//import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
-//import { faHeart as emptyHeart } from '@fortawesome/free-regular-svg-icons';
 
-import {paginate} from "../utils/paginate.js"
+import { paginate } from "../utils/paginate.js";
+
+import ListGroup from "./common/listGroup";
 class Movies extends Component {
   state = {
     movies: getMovies(),
@@ -43,77 +49,70 @@ class Movies extends Component {
     this.setState({ movies });
   };
   handlePageChange = (page) => {
-    
-    this.setState({ currentPage:page})
-
-    
+    this.setState({ currentPage: page });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies} = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
     if (!this.state.movies.length)
       return <p>There are no movies in the database</p>;
 
     const movies = paginate(allMovies, currentPage, pageSize);
-    
+
     return (
       <React.Fragment>
-        <span className="span-4">
-            <ul className="list-group">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Morbi leo risus</li>
-            <li className="list-group-item">Porta ac consectetur ac</li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
-        </span>
+        <Row>
+          <Col md="3">
+            <ListGroup></ListGroup>
+          </Col>
 
-        <span className="span-8">
-          <p>Showing {count} movies in the database.</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th>Like</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate} </td>
-                  <td>
-                    <Like
-                      onClick={() => this.handleLike(movie)}
-                      liked={movie.liked}
-                    ></Like>
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
+          <Col className="container">
+            <p>Showing {count} movies in the database.</p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rate</th>
+                  <th>Like</th>
+                  <th></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <Pagination
-            itemsCount={count}
-            pageSize={pageSize}
-            currentPage={currentPage}
-            onPageChange={this.handlePageChange}
-          ></Pagination>
-        </span>
+              </thead>
+              <tbody>
+                {movies.map((movie) => (
+                  <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate} </td>
+                    <td>
+                      <Like
+                        onClick={() => this.handleLike(movie)}
+                        liked={movie.liked}
+                      ></Like>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => this.handleDelete(movie)}
+                        className="btn btn-danger btn-sm"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              itemsCount={count}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+            ></Pagination>
+          </Col>
+        </Row>
       </React.Fragment>
     );
   }
